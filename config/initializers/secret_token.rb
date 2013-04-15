d14aff7af56582568021636fe48c90851d5952be
cis <cis@CISM135.(none)>
2013-04-15 12:49:54 +0530

    more nonsense

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '7c21ca6a477c8ae836d61fee289bdff3cc434c640e74a23d9d195e88ed79fcf47c46183bc503130dcefe160d47962f1c595a2261f0df456d61c74a3928f4c6b0'
